5e3d6e65388714636195516e98548f2dbf21837b
Dylan Michael Labat <dml252@live.com>
2018-12-21 23:15:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == !phrase.upcase
+  if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
